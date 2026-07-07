8947a770230288aa2be9f6ea6a51c54ee6be691d
PastMaster1 <261282061+PastMaster1@users.noreply.github.com>
2026-07-07 11:44:50 +0500

    Initial commit

---
+++ AlgorithmsOnC#/StructureMinStack.cs
@@ -0,0 +1,94 @@
+﻿namespace AlgorithmsOnCs;
+
+public class MinStack
+{
+    private class Node
+    {
+        public int data;
+        public Node? previous;
+        public int min_data;
+        public int count;
+        public Node(int value)
+        {
+            data = value;
+            previous = null;
+        }
+    }
+    private Node? current;
+    public MinStack()
+    {
+        current = null;
+    }
+    public void Push(int value)
+    {
+        Node node = new Node(value);
+        if (!IsEmpty())
+        {
+            node.previous = current;
+            node.min_data = ((value < current!.min_data) ? value : current!.min_data);
+            node.count = current.count + 1;
+        }
+        else
+        {
+            node.min_data = value;
+            node.count = 1;
+        }
+        current = node;
+    }
+    public bool IsEmpty()
+    {
+        return (current == null);
+    }
+    public void Pop()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+        else
+        {
+            current = current!.previous;
+        }
+    }
+    public int Top()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+        return current!.data;
+    }
+    public int GetMin()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+        return current!.min_data;
+    }
+    public int Count()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+        return current!.count;
+    }
+    public void PrintStack()
+    {
+        if (IsEmpty())
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Node cur = current!;
+            while (cur.previous != null)
+            {
+                Console.WriteLine(cur.data);
+                cur = cur.previous;
+            }
+            Console.WriteLine(cur.data);
+        }
+    }
+}
---
+++ MinStack.Tests/StructureMinStackTests.cs
@@ -0,0 +1,153 @@
+﻿using Xunit;
+using AlgorithmsOnCs;
+namespace MinStackTests;
+
+public class StructureMinStackTests
+{
+    [Fact]
+    public void PrintEmptyStack()
+    {
+        using (var stringWriter = new StringWriter())
+        {
+            var originalOutput = Console.Out;
+            Console.SetOut(stringWriter);
+            try
+            {
+                MinStack stack = new MinStack();
+                stack.PrintStack();
+                var actualOutput = stringWriter.ToString();
+                Assert.Equal($"Stack is empty{Environment.NewLine}", actualOutput);
+            }
+            finally
+            {
+                Console.SetOut(originalOutput);
+            }
+        }
+    }
+
+    [Fact]
+    public void PrintNotEmptyStack()
+    {
+        using (var stringWriter = new StringWriter())
+        {
+            var originalOutput = Console.Out;
+            Console.SetOut(stringWriter);
+            try
+            {
+                MinStack stack = new MinStack();
+                stack.Push(1);
+                stack.Push(2);
+                stack.Push(3);
+                stack.PrintStack();
+                var actualOutput = stringWriter.ToString();
+                Assert.Equal($"3{Environment.NewLine}2{Environment.NewLine}1{Environment.NewLine}", actualOutput);
+            }
+            finally
+            {
+                Console.SetOut(originalOutput);
+            }
+        }
+    }
+
+    [Fact]
+    public void CalculateSizeOfEmptyStack()
+    {
+        MinStack stack = new MinStack();
+        var exception = Assert.Throws<InvalidOperationException>(() =>
+        {
+            stack.Count();
+        });
+        Assert.Equal("Stack is empty", exception.Message);
+    }
+    
+    [Fact]
+    public void CalculateSizeOfNotEmptyStack()
+    {
+        MinStack stack = new MinStack();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        int size = stack.Count();
+        Assert.Equal(3, size);
+    }
+
+    [Fact]
+    public void TopElementOfEmptyStack()
+    {
+        MinStack stack = new MinStack();
+        var exception = Assert.Throws<InvalidOperationException>(() =>
+        {
+            stack.Top();
+        });
+        Assert.Equal("Stack is empty", exception.Message);
+    }
+
+    [Fact]
+    public void TopElementOfNotEmptyStack()
+    {
+        MinStack stack = new MinStack();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        int d = stack.Top();
+        Assert.Equal(3, d);
+    }
+
+    [Fact]
+    public void DeleteElementFromEmptyStack()
+    {
+        MinStack stack = new MinStack();
+        var exception = Assert.Throws<InvalidOperationException>(() =>
+        {
+            stack.Pop();
+        });
+        Assert.Equal("Stack is empty", exception.Message);
+    }
+
+    [Fact]
+    public void DeleteElementFromNotEmptyStack()
+    {
+        using (var stringWriter = new StringWriter())
+        {
+            var originalOutput = Console.Out;
+            Console.SetOut(stringWriter);
+            try
+            {
+                MinStack stack = new MinStack();
+                stack.Push(1);
+                stack.Push(2);
+                stack.Push(3);
+                stack.Pop();
+                stack.PrintStack();
+                var actualOutput = stringWriter.ToString();
+                Assert.Equal($"2{Environment.NewLine}1{Environment.NewLine}", actualOutput);
+            }
+            finally
+            {
+                Console.SetOut(originalOutput);
+            }
+        }
+    }
+
+    [Fact]
+    public void GetMinValueFromEmptyStack()
+    {
+        MinStack stack = new MinStack();
+        var exception = Assert.Throws<InvalidOperationException>(() =>
+        {
+            stack.GetMin();
+        });
+        Assert.Equal("Stack is empty", exception.Message);
+    }
+
+    [Fact]
+    public void GetMinValueFromNotEmptyStack()
+    {
+        MinStack stack = new MinStack();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        int minValue = stack.GetMin();
+        Assert.Equal(1, minValue);
+    }
+}
